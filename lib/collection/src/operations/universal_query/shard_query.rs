0308c347203acc331bc218a5310b11edda2cb755
Luis Cossío <luis.cossio@outlook.com>
2025-10-10 14:23:21 -0300

    rename `NamedQuery::new_from_vector` -> `NamedQuery::new

--- lib/collection/src/operations/universal_query/shard_query.rs
+++ lib/collection/src/operations/universal_query/shard_query.rs
@@ -336,15 +336,17 @@ fn query_enum_from_grpc_raw_query(
                     DEFAULT_VECTOR_NAME.to_owned()
                 }
             };
-            let named_vector = NamedQuery::new_from_vector(vector, name);
+            let named_vector = NamedQuery::new(vector, name);
             QueryEnum::Nearest(named_vector)
         }
-        Variant::RecommendBestScore(recommend) => {
-            QueryEnum::RecommendBestScore(NamedQuery::new(RecoQuery::try_from(recommend)?, using))
-        }
-        Variant::RecommendSumScores(recommend) => {
-            QueryEnum::RecommendSumScores(NamedQuery::new(RecoQuery::try_from(recommend)?, using))
-        }
+        Variant::RecommendBestScore(recommend) => QueryEnum::RecommendBestScore(NamedQuery {
+            query: RecoQuery::try_from(recommend)?,
+            using,
+        }),
+        Variant::RecommendSumScores(recommend) => QueryEnum::RecommendSumScores(NamedQuery {
+            query: RecoQuery::try_from(recommend)?,
+            using,
+        }),
         Variant::Discover(discovery) => QueryEnum::Discover(NamedQuery {
             query: DiscoveryQuery::try_from(discovery)?,
             using,
--- lib/segment/src/data_types/vectors.rs
+++ lib/segment/src/data_types/vectors.rs
@@ -772,10 +772,10 @@ impl NamedQuery<VectorInternal> {
     }
 }
 
-impl<TVector> NamedQuery<TVector> {
-    pub fn new_from_vector(vector: TVector, using: impl Into<String>) -> NamedQuery<TVector> {
+impl<TQuery> NamedQuery<TQuery> {
+    pub fn new(query: TQuery, using: impl Into<String>) -> NamedQuery<TQuery> {
         NamedQuery {
-            query: vector,
+            query,
             using: Some(using.into()),
         }
     }
@@ -813,13 +813,6 @@ impl<T> Named for NamedQuery<T> {
 impl<T: Validate> Validate for NamedQuery<T> {
     fn validate(&self) -> Result<(), validator::ValidationErrors> {
         self.query.validate()
-    }
-}
-
-impl NamedQuery<RecoQuery<VectorInternal>> {
-    pub fn new(query: RecoQuery<VectorInternal>, using: Option<VectorNameBuf>) -> Self {
-        // TODO: maybe validate there is no sparse vector without vector name
-        NamedQuery { query, using }
     }
 }
 
